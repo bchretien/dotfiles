3f4e7fd7270b6bd32a1b4802016467b4020d8c13
Benjamin Chrétien <chretien@lirmm.fr>
2013-12-29 15:17:58 +0100

    Use .ini files.

--- setup.sh
+++ setup.sh
@@ -5,26 +5,35 @@
 set -e
 
 if ! `test x$(basename $(pwd)) = x.dotfiles`; then
- echo "please run ./setup.sh from .dotfiles directory"
- exit 1
+    echo "please run ./setup.sh from .dotfiles directory"
+    exit 1
 fi
+
+# Files ending with .ini are copied if they do not already exist
+inifiles=`find . -maxdepth 1 -type f -and -name '*.ini'`
+for i in $inifiles; do
+    new_name=${i%.ini}
+    if [ ! -f $new_name ]; then
+        cp $i $new_name
+    fi
+done
 
 # Files starting with .
 dotfiles=`find . -maxdepth 1 -type f -and -name '.*'`
 for i in $dotfiles; do
- ln -sf `pwd`/$i $HOME/$i
+    ln -sf `pwd`/$i $HOME/$i
 done
 
 # Links starting with .
 dotlinks=`find . -maxdepth 1 -type l -and -name '.*'`
 for i in $dotlinks; do
- ln -sf `pwd`/$i $HOME/$i
+    ln -sf `pwd`/$i $HOME/$i
 done
 
 # Directories starting with . (except . and .git)
 dotdirs=`find . -maxdepth 1 -type d -and -name '.*' -and -not \( -path "./.git" -o -path "." \)`
 for i in $dotdirs; do
- ln -sf `pwd`/$i $HOME/$i
+    ln -sf `pwd`/$i $HOME/$i
 done
 
 # Install spf13-vim
