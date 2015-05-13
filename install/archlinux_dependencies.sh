2c36c7d2b95b5aaf052d6d1fc5895f1635d95697
Benjamin Chrétien <chretien@lirmm.fr>
2015-05-13 20:42:14 +0900

    Clean install script/direcctory.

--- install/archlinux_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install official dependencies
-sudo pacman -S zsh git autojump gvim tmux ctags ack
-
-# Install AUR dependencies
-yaourt --noconfirm libtinfo
--- install/debian_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-sudo apt-get install zsh git autojump vim tmux exuberant-ctags ack-grep
