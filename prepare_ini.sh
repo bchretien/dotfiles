4ecdd3d1c05f635e3a6f9dc3eaa411bf30c90987
Benjamin Chrétien <chretien@lirmm.fr>
2015-05-13 10:36:09 +0900

    dotbot: add support for ini files.

---
+++ prepare_ini.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+dotdir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+# Files ending with .ini are copied if they do not already exist
+inifiles=`find $dotdir -maxdepth 1 -type f -and -name '*.ini'`
+for i in $inifiles; do
+  new_name=${i%.ini}
+  if [ ! -f $new_name ]; then
+    cp $i $new_name
+  fi
+done
+
+
+# For ini files in config
+config_inifiles=`find $dotdir/config -maxdepth 1 -type f -and -name '*.ini'`
+for i in $config_inifiles; do
+  ii=${i#./}
+  new_name=${ii%.ini}
+
+  # Copy ini file if does not already exist
+  if [ ! -f $new_name ]; then
+    cp $i $new_name
+  fi
+
+  # Linking done by dotbot
+done
