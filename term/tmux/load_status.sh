63399ffa3e812228e0b0875ef2afe474e134967b
Benjamin Chrétien <chretien@lirmm.fr>
2015-07-03 01:34:33 +0900

    tmux: source status files instead

--- term/tmux/load_status.sh
+++ term/tmux/load_status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if ([ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]) && [ -f ~/.tmux/status_ssh.tmux ]; then
-  ~/.tmux/status_ssh.tmux
+  tmux source-file ~/.tmux/status_ssh.tmux >/dev/null 2>&1
 elif [ -f ~/.tmux/status.tmux ]; then
-  ~/.tmux/status.tmux
+  tmux source-file ~/.tmux/status.tmux >/dev/null 2>&1
 fi
