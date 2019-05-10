f6b1a57a84898f54bc6ca276520b5902cf0f27c1
Benjamin Chrétien <chretien.b@gmail.com>
2019-05-10 23:53:21 +0200

    Update config

--- bin/compress_video.sh
+++ bin/compress_video.sh
@@ -8,7 +8,7 @@ display_help() {
   exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
     echo "Error: illegal number of parameters"
     echo
     display_help
@@ -16,11 +16,13 @@ fi
 
 input_video=$1
 output_video=$2
+shift
+shift
 
-ffmpeg -y -i $input_video -c:v libx264 -preset medium -b:v 555k \
+ffmpeg -y -i "$input_video" -c:v libx264 -preset medium -b:v 555k $@ \
        -pass 1 -strict -2 -f mp4 /dev/null
-ffmpeg -i $input_video -c:v libx264 -preset medium -b:v 555k \
-       -pass 2 -strict -2 $output_video
+ffmpeg -i "$input_video" -c:v libx264 -preset medium -b:v 555k $@ \
+       -pass 2 -strict -2 "$output_video"
 
 # Remove ffmpeg files
 rm ffmpeg2pass-0.log
